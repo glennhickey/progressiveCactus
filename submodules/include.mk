ec2eddf75352327da774f90182b9d3d03bf3a57e
Glenn Hickey <glenn.hickey@gmail.com>
2014-05-15 12:33:51 -0400

    switch out CURDIR for PWD to keep symlinks in paths which will help them be usable to get around spaces in paths issues

--- submodules/include.mk
+++ submodules/include.mk
@@ -1,17 +1,17 @@
 #Modify this variable to set the location of sonLib
-sonLibRootPath=$(CURDIR)/sonLib
+sonLibRootPath=$(PWD)/sonLib
 sonLibPath=${sonLibRootPath}/lib
 
 
 #OVERRIDE SONLIB WITH LOCAL DBS (because now sonlib won't redefine if exist)
-tcPrefix = $(CURDIR)/tokyocabinet
+tcPrefix = $(PWD)/tokyocabinet
 tokyoCabinetIncl = -I ${tcPrefix}/include -DHAVE_TOKYO_CABINET=1
 tokyoCabinetLib = -L${tcPrefix}/lib -Wl,-rpath,${tcPrefix}/lib -ltokyocabinet -lz -lpthread -lm
 
-kcPrefix =$(CURDIR)/kyotocabinet
-ttPrefix =$(CURDIR)/kyototycoon
-kyotoTycoonIncl = -I${kcPrefix}/include -I${ttPrefix}/include -DHAVE_KYOTO_TYCOON=1 -I$(CURDIR)/zlib/include 
-kyotoTycoonLib = -L$(CURDIR)/zlib/lib -L${ttPrefix}/lib -Wl,-rpath,${ttPrefix}/lib -lkyototycoon -L${kcPrefix}/lib -Wl,-rpath,${kcPrefix}/lib -lkyotocabinet -Wl,-rpath,$(CURDIR)/zlib/lib -lz -lpthread -lm -lstdc++
+kcPrefix =$(PWD)/kyotocabinet
+ttPrefix =$(PWD)/kyototycoon
+kyotoTycoonIncl = -I${kcPrefix}/include -I${ttPrefix}/include -DHAVE_KYOTO_TYCOON=1 -I$(PWD)/zlib/include 
+kyotoTycoonLib = -L$(PWD)/zlib/lib -L${ttPrefix}/lib -Wl,-rpath,${ttPrefix}/lib -lkyototycoon -L${kcPrefix}/lib -Wl,-rpath,${kcPrefix}/lib -lkyotocabinet -Wl,-rpath,$(PWD)/zlib/lib -lz -lpthread -lm -lstdc++
 
 #DISABLE MYSQUL
 mysqlIncl = 
@@ -29,18 +29,18 @@ basicLibs = ${sonLibPath}/sonLib.a ${sonLibPath}/cuTest.a ${dblibs}
 basicLibsDependencies = ${sonLibPath}/sonLib.a ${sonLibPath}/cuTest.a 
 
 #put hdf5 at beginning of path
-h5path = $(CURDIR)/hdf5
+h5path = $(PWD)/hdf5
 PATH := ${h5path}/bin:${PATH}
 h5prefix = -prefix=${h5path}
 
 #environment
-myEnv = $(CURDIR)/../environment
+myEnv = $(PWD)/../environment
 
 #kyoto tycoon et al have problems with shared libraries on the cluster
 #but shared libraries seem to be necessary to build on osx.
-LDFLAGS := -L$(CURDIR)/zlib/lib -L$(CURDIR)/kyotocabinet/lib $(LDFLAGS)
+LDFLAGS := -L$(PWD)/zlib/lib -L$(PWD)/kyotocabinet/lib $(LDFLAGS)
 CXXFLAGS := $(cppflags) $(CXXFLAGS)
-LD_LIBRARY_PATH := $(CURDIR)/zlib/lib:$(CURDIR)/kyotocabinet/lib:$(CURDIR)/kyototycoon/lib:$(LD_LIBRARY_PATH)
+LD_LIBRARY_PATH := $(PWD)/zlib/lib:$(PWD)/kyotocabinet/lib:$(PWD)/kyototycoon/lib:$(LD_LIBRARY_PATH)
 #UNAME := $(shell uname)
 #ifeq ($(UNAME), Darwin)
 ktlinkingflags =
